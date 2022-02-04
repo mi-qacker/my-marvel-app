81f9a30f405e13ac675cb781787d147c72e65a8a
Матвей Калинин <matvey.kalinin2001@gmail.com>
2022-02-04 22:43:24 +0300

    create service

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MarvelService from './services/MarvelService';
 import App from './components/app/App';
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+marvelService
+  .getAllCharacters()
+  .then((res) =>
+    res.data.results.forEach((element) => console.log(element.name))
+  );
 
 ReactDOM.render(
   <React.StrictMode>
