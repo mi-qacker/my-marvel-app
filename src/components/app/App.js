c9abc769a44829dcdc1463ede8888eda7bc34a21
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2022-02-04 15:51:27 +0300

    add layout project

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import AppHeader from '../appHeader/AppHeader';
+import CharInfo from '../charInfo/CharInfo';
+import CharList from '../charList/CharList';
+import RandomChar from '../randomChar/RandomChar';
+
+import decoration from '../../resources/img/vision.png';
+
+const App = () => {
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <RandomChar />
+        <div className="char__content">
+          <CharList />
+          <CharInfo />
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  );
+};
+
+export default App;
