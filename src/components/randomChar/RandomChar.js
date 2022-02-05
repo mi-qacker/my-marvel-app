fbb831c81e7da76b1c004b5bb909af27cffe2bf0
Матвей Калинин <matvey.kalinin2001@gmail.com>
2022-02-05 18:54:29 +0300

    add spinner

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import Spinner from '../spinner/Spinner';
 import MarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
@@ -12,11 +13,12 @@ class RandomChar extends Component {
 	}
 	state = {
 		char: {},
+		loading: true,
 	};
 	marvelService = new MarvelService();
 
-	onCharLoaded = char => {
-		this.setState({ char });
+	onCharLoaded = (char) => {
+		this.setState({ char, loading: false });
 	};
 
 	updateChar = () => {
@@ -27,39 +29,10 @@ class RandomChar extends Component {
 	};
 
 	render() {
-		const {
-			char: { name, description, thumbnail, homepage, wiki },
-		} = this.state;
-		const maxWord = 30;
-		let desc = '[NOT FOUND]';
-		if (description) {
-			const descWords = description.split(' ');
-			desc =
-				descWords.length > maxWord
-					? [...descWords.slice(0, maxWord), '...'].join(' ')
-					: description;
-		}
+		const { char, loading } = this.state;
 		return (
 			<div className="randomchar">
-				<div className="randomchar__block">
-					<img
-						src={thumbnail}
-						alt="Random character"
-						className="randomchar__img"
-					/>
-					<div className="randomchar__info">
-						<p className="randomchar__name">{name}</p>
-						<p className="randomchar__descr">{desc}</p>
-						<div className="randomchar__btns">
-							<a href={homepage} className="button button__main">
-								<div className="inner">homepage</div>
-							</a>
-							<a href={wiki} className="button button__secondary">
-								<div className="inner">Wiki</div>
-							</a>
-						</div>
-					</div>
-				</div>
+				{loading ? <Spinner /> : <View char={char} />}
 				<div className="randomchar__static">
 					<p className="randomchar__title">
 						Random character for today!
@@ -80,5 +53,39 @@ class RandomChar extends Component {
 		);
 	}
 }
+
+const View = ({ char }) => {
+	const { name, description, thumbnail, homepage, wiki } = char;
+	const maxWord = 30;
+	let desc = '[NOT FOUND]';
+	if (description) {
+		const descWords = description.split(' ');
+		desc =
+			descWords.length > maxWord
+				? [...descWords.slice(0, maxWord), '...'].join(' ')
+				: description;
+	}
+	return (
+		<div className="randomchar__block">
+			<img
+				src={thumbnail}
+				alt="Random character"
+				className="randomchar__img"
+			/>
+			<div className="randomchar__info">
+				<p className="randomchar__name">{name}</p>
+				<p className="randomchar__descr">{desc}</p>
+				<div className="randomchar__btns">
+					<a href={homepage} className="button button__main">
+						<div className="inner">homepage</div>
+					</a>
+					<a href={wiki} className="button button__secondary">
+						<div className="inner">Wiki</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default RandomChar;
