7583520584d632c7be402a512235d0c26012fa0d
Матвей Калинин <matvey.kalinin2001@gmail.com>
2022-02-20 14:14:52 +0300

    add react-hook-form

--- src/components/charSearch/CharSearch.js
+++ src/components/charSearch/CharSearch.js
@@ -1,19 +1,34 @@
+import { useForm } from 'react-hook-form';
 import './charSearch.scss';
 
 const CharSearch = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
 	return (
 		<div className="char__search">
 			<div className="char__search_title">
 				Or find a character by name:
 			</div>
-			<input
-				className="char__input"
-				type="text"
-				placeholder="Enter name"
-			/>
-			<a className="button button__main">
-				<div className="inner">find</div>
-			</a>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<input
+					{...register('name', { required: true })}
+					className="char__input"
+					type="text"
+					placeholder="Enter name"
+				/>
+				<button className="button button__main" type="submit">
+					<div className="inner">find</div>
+				</button>
+			</form>
+			{errors.name ? (
+				<div className="char__search_result">
+					<div className="error">This field is required</div>
+				</div>
+			) : null}
 			{/*<div className="char__search_result">
 				<div className="success">There is! Visit name page?</div>
 				<a className="button button__secondary">
@@ -24,9 +39,6 @@ const CharSearch = () => {
 				<div className="error">
 					The character was not found. Check the name and try again
 				</div>
-			</div>*/}
-			{/*<div className="char__search_result">
-				<div className="error">This field is required</div>
 			</div>*/}
 		</div>
 	);
