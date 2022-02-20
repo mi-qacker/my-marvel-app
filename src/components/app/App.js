36c7d4a5e1bdf2df332640a73b62832592397d16
Матвей Калинин <matvey.kalinin2001@gmail.com>
2022-02-20 16:27:07 +0300

    test to deploy add lazy again

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,18 @@
-import { Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import AppHeader from '../appHeader/AppHeader';
 import Spinner from '../spinner/Spinner';
-import MainPage from '../pages/mainPage/MainPage';
-import ComicsPage from '../pages/comicsPage/ComicsPage';
-import SingleComicPage from '../pages/singleComicPage/SingleComicPage';
-import SingleCharPage from '../pages/singleCharPage/SingleCharPage';
-import NotFoundPage from '../pages/notFoundPage/NotFoundPage';
 
-// const NotFoundPage = lazy(() => import('../pages/notFoundPage/NotFoundPage'));
-// const MainPage = lazy(() => import('../pages/mainPage/MainPage'));
-// const ComicsPage = lazy(() => import('../pages/comicsPage/ComicsPage'));
-// const SingleComicPage = lazy(() =>
-// 	import('../pages/singleComicPage/SingleComicPage')
-// );
-// const SingleCharPage = lazy(() =>
-// 	import('../pages/singleCharPage/SingleCharPage')
-// );
+const NotFoundPage = lazy(() => import('../pages/notFoundPage/NotFoundPage'));
+const MainPage = lazy(() => import('../pages/mainPage/MainPage'));
+const ComicsPage = lazy(() => import('../pages/comicsPage/ComicsPage'));
+const SingleComicPage = lazy(() =>
+	import('../pages/singleComicPage/SingleComicPage')
+);
+const SingleCharPage = lazy(() =>
+	import('../pages/singleCharPage/SingleCharPage')
+);
 
 const App = () => {
 	return (
